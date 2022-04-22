e3a5275cdc65613c82755ea7734c44f72be84fee
Shiju Nambiar <skoodath@gmail.com>
2022-04-22 21:57:01 +0530

    destructuring changes

--- css-demo/script.js
+++ css-demo/script.js
@@ -3,25 +3,25 @@ const domElements = {
   box_after: document.querySelector(".css_description_before"),
 };
 
-const { box_before, box_after } = domElements;
+const { box_before: before, box_after: after } = domElements;
 
 document.addEventListener("DOMContentLoaded", () => {
   const box = document.querySelector(".box_sizing");
   if (box.classList.contains("box-size")) {
-    box_before.style.display = "none";
-    box_after.style.display = "flex";
+    before.style.display = "none";
+    after.style.display = "flex";
   } else {
-    box_before.style.display = "flex";
-    box_after.style.display = "none";
+    before.style.display = "flex";
+    after.style.display = "none";
   }
 });
 const boxSizing = (el) => {
   el.classList.toggle("box-size");
   if (el.classList.contains("box-size")) {
-    box_before.style.display = "none";
-    box_after.style.display = "flex";
+    before.style.display = "none";
+    after.style.display = "flex";
   } else {
-    box_before.style.display = "flex";
-    box_after.style.display = "none";
+    before.style.display = "flex";
+    after.style.display = "none";
   }
 };
